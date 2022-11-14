65784a10d0a2f98a6eefde8256d2a9f2913874de
Ahmed Wagih <ahmd.wagih96@gmail.com>
2022-11-14 21:32:33 +0200

    added search term from input and redirect to search page

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,24 @@
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 function Body() {
+  const router = useRouter();
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search?term=${input.trim()}`);
+  };
+  const randomSearch = (e) => {
+    e.preventDefault();
+    console.log("random");
+  };
+
   return (
-    <form className="flex flex-col items-center mt-40">
+    <form className="flex flex-col items-center mt-40" onSubmit={search}>
       <Image
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
         width="300"
@@ -12,12 +28,23 @@ function Body() {
       />
       <div className="flex w-[90%] mx-auto px-5 py-3 max-w-[600px] rounded-full mt-5 items-center border border-gray-200 hover:shadow-lg focus-within:shadow-lg gap-3">
         <SearchIcon className="h-5 text-gray-500" />
-        <input type="text" className="border-none outline-none flex-grow" />
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          type="text"
+          className="border-none outline-none flex-grow"
+        />
         <MicrophoneIcon className="h-5" />
       </div>
       <div className="flex flex-col sm:flex-row gap-2 mt-5">
-        <button className="search-btn">Google Search</button>
-        <button className="search-btn">I'm Feeling Lucky</button>
+        <button type="submit" className="search-btn">
+          Google Search
+        </button>
+        <button className="search-btn" onClick={randomSearch}>
+          I'm Feeling Lucky
+        </button>
       </div>
     </form>
   );
