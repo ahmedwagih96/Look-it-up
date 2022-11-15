b16d0143d0503f9898c3f4b2ca1f5fa0d9156748
Ahmed Wagih <ahmd.wagih96@gmail.com>
2022-11-15 22:36:57 +0200

    Created Search Page Header Component

---
+++ components/SearchHeader.js
@@ -0,0 +1,51 @@
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import User from "./User";
+
+function SearchHeader() {
+  const router = useRouter();
+  const [input, setInput] = useState(router.query.term);
+  const submitSearch = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search?term=${input.trim()}`);
+  };
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+          width="120"
+          height="40"
+          className="object-contain cursor-pointer"
+          alt="google logo"
+          onClick={() => router.push("/")}
+        />
+        <form
+          className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+          onSubmit={submitSearch}
+        >
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="border-none outline-none flex-grow"
+          />
+          <XIcon
+            className="h-7 text-gray-500 cursor-pointer md:mr-3"
+            onClick={() => {
+              setInput("");
+            }}
+          />
+          <MicrophoneIcon className="h-6 hidden md:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+          <SearchIcon className="h-6 hidden md:inline-flex text-blue-500 " />
+        </form>
+        <User className="ml-auto whitespace-nowrap" />
+      </div>
+    </header>
+  );
+}
+
+export default SearchHeader;
--- components/User.js
+++ components/User.js
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-function User() {
+function User({ className }) {
   const { data: session } = useSession();
   if (session) {
     return (
@@ -9,14 +9,19 @@ function User() {
           src={session.user.image}
           alt="user image"
           onClick={signOut}
-          className="rounded-full h-10 w-10 hover:bg-gray-200 p-1 cursor-pointer"
+          className={`rounded-full h-10 w-10 hover:bg-gray-200 p-1 cursor-pointer ${className}`}
         />
       </>
     );
   }
   return (
     <>
-      <button onClick={signIn} className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:shadow-md hover:bg-blue-400">Sign In</button>
+      <button
+        onClick={signIn}
+        className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:shadow-md hover:bg-blue-400 ${className}`}
+      >
+        Sign In
+      </button>
     </>
   );
 }
