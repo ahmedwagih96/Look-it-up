4dce7955ae1f0e4e7faa5b9df95b1526e6ccc809
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-06-25 12:28:02 +0300

    removed dummy data

--- pages/search.js
+++ pages/search.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import SearchHeader from "../components/SearchHeader";
 import SearchResults from "../components/SearchResults";
-import Response from "../Response";
 
 function Search({ results }) {
   const router = useRouter();
@@ -24,17 +23,12 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const startIndex = context.query.start || "1";
-  const mockData = false;
   const { term, searchType } = context.query;
-  const data = mockData
-    ? Response
-    : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${
-          process.env.API_KEY
-        }&cx=${process.env.CONTEXT_KEY}&q=${term}${
-          searchType && "&searchType=image"
-        }&start=${startIndex}`
-      ).then((response) => response.json());
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
+      process.env.CONTEXT_KEY
+    }&q=${term}${searchType && "&searchType=image"}&start=${startIndex}`
+  ).then((response) => response.json());
   return {
     props: {
       results: data,
